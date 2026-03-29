a9a47c8f44ae316c0dcef3afc5d02032aec60f56
Özge Bostan <ozgebostan03@gmail.com>
2026-03-29 16:36:45 +0300

    MovieCard class to be used in WatchversePanel

--- src/main/java/Client/UI/components/MovieCard.java
+++ src/main/java/Client/UI/components/MovieCard.java
@@ -1,4 +1,22 @@
 package Client.UI.components;
 
-public class MovieCard {
+import Model.Item;
+
+import javax.swing.*;
+import java.util.function.Consumer;
+
+public class MovieCard extends JButton {
+
+    private final Item item;
+
+    public MovieCard(Item item, Consumer<Item> onDetailsClick) {
+        this.item = item;
+
+        setupUI();
+
+        addActionListener(_ -> onDetailsClick.accept(item));
+    }
+
+    private void setupUI() {
+    }
 }
